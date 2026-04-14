d6e83c053fb2cbc7e270bfd4297c3a9ee4609a00
Eric Moss <1670755+dohnutt@users.noreply.github.com>
2026-04-13 21:29:54 -0400

    Add meta/og data

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/script.js");
     eleventyConfig.addPassthroughCopy("src/style.css");
+    eleventyConfig.addPassthroughCopy("src/share-image.png");
 
     return {
         dir: {
--- src/restaurants/restaurants.11tydata.js
+++ src/restaurants/restaurants.11tydata.js
@@ -1,6 +1,7 @@
 module.exports = {
     layout: "restaurant.njk",
     eleventyComputed: {
+        name: (data) => data.title,
         permalink: (data) => data.ignore ? false : data.permalink
     }
 };
